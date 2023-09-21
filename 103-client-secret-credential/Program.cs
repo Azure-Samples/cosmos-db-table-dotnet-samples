2abca050161b858ea53df0387398ad3891233802
Sidney Andrews <sidandrews@microsoft.com>
2023-09-21 12:45:57 -0400

    Migrate existing samples

---
+++ 103-client-secret-credential/Program.cs
@@ -0,0 +1,25 @@
+
+// <using_identity_directives> 
+using Azure.Core;
+using Azure.Identity;
+// </using_identity_directives>// <using_directives> 
+using Azure.Data.Tables;
+// </using_directives>
+
+// <credential>
+// Custom credential class for servers and clients outside of Azure
+TokenCredential credential = new ClientSecretCredential(
+    tenantId: Environment.GetEnvironmentVariable("AAD_TENANT_ID")!,
+    clientId: Environment.GetEnvironmentVariable("AAD_CLIENT_ID")!,
+    clientSecret: Environment.GetEnvironmentVariable("AAD_CLIENT_SECRET")!,
+    options: new TokenCredentialOptions()
+);
+// </credential>
+
+// <secret_credential> 
+// New instance of TableServiceClient class using a credential
+TableServiceClient client = new(
+    endpoint: new Uri(Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!),
+    tokenCredential: credential
+);
+// </secret_credential> 
---
+++ 275-read-item-tableentity/Program.cs
@@ -0,0 +1,47 @@
+// <using_directives> 
+using Azure.Data.Tables;
+// </using_directives>
+
+// <connection_string> 
+// New instance of TableServiceClient class using a connection string
+TableServiceClient client = new(
+    connectionString: Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING")!
+);
+// </connection_string>
+
+// <get_table>
+// Get instance of TableClient class referencing the server-side table
+TableClient tableClient = client.GetTableClient(
+    tableName: "adventureworks"
+);
+// </get_table>
+
+// <create_object> 
+// Create dictionary
+Dictionary<string, object> properties = new()
+{
+    { "RowKey", "68719518388" },
+    { "PartitionKey", "gear-surf-surfboards" },
+    { "Name", "Sunnox Surfboard" },
+    { "Quantity", 8 },
+    { "Sale", true }
+};
+
+// Create new item using dictionary constructor
+TableEntity newItem = new(
+    values: properties
+);
+// </create_object>
+
+// <create_item>
+// Add new item to server-side table
+await tableClient.AddEntityAsync<TableEntity>(newItem);
+// </create_item>
+
+// <read_item>
+// Read existing item from server-side table
+TableEntity readItem = await tableClient.GetEntityAsync<TableEntity>(
+    partitionKey: "68719518388",
+    rowKey: "gear-surf-surfboards"
+);
+// </read_item>
